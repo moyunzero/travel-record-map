e5f3653202a90f67ad16ddfdd86e619f53fa58dc
moyun <zero305747648@gmail.com>
2026-04-23 17:06:55 +0800

    feat:增加深浅模式

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -7,6 +7,7 @@
     </div>
 
     <div class="navbar-end">
+      <AppThemeToggle />
       <a class="btn btn-accent">登录</a>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode'],
   vite: {
     plugins: [
       tailwindcss(),
@@ -16,5 +16,8 @@ export default defineNuxtConfig({
         '@vue/devtools-kit',
       ],
     },
+  },
+  colorMode: {
+    dataValue: 'theme',
   },
 })
