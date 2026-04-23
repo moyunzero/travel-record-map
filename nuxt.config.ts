b2d805badcab99b6ae9e31027dd131b69589d4f8
moyun <zero305747648@gmail.com>
2026-04-23 14:40:19 +0800

    feat:增加样式&图标

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  css: ['./app/assets/css/main.css'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
   vite: {
+    plugins: [
+      tailwindcss(),
+    ],
     optimizeDeps: {
       include: [
         '@vue/devtools-core',
